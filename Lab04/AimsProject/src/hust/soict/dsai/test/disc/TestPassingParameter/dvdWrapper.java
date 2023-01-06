4e61905232a438230847f6bb2b57824efa06c401
dlnh13 <97794244+dlnh13@users.noreply.github.com>
2023-01-06 17:52:00 +0700

    lab05

--- Lab04/AimsProject/src/hust/soict/dsai/test/disc/TestPassingParameter/dvdWrapper.java
+++
@@ -1,9 +0,0 @@
-package hust.soict.dsai.test.disc.TestPassingParameter;
-import hust.soict.dsai.aims.media.DigitalVideoDisc;
-
-public class dvdWrapper {
-	DigitalVideoDisc c;
-	public dvdWrapper(DigitalVideoDisc c) {
-		this.c = c;
-	}
-}
